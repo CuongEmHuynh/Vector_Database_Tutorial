760ad13436279b3d8a26850213caa1ce04db89b5
CuongEmHuynh <em.huynh@eiu.edu.vn>
2025-10-21 16:36:00 +0700

    # fetch data notes

--- Advising/database/sql_client.py
+++ Advising/database/sql_client.py
@@ -1,6 +1,36 @@
-from Advising.config import settings
+import numpy as np
+import pyodbc
+import pandas as pd
 
-print("Database Settings:")
-print(f"Server DB: {settings.SERVERDB}")
-print(f"Username: {settings.USERNAME}")
-print(f"Password: {settings.PASSWORD}") 
+SERVERDB="192.168.6.54"
+USERNAME="sa"
+PASSWORD="p@ssw0rd789"
+
+# sql_client.py
+import pyodbc
+
+def get_connection():
+    conn = pyodbc.connect(
+        f"DRIVER={{ODBC Driver 17 for SQL Server}};"
+        f"SERVER={SERVERDB};"
+        f"DATABASE=CoVan;"
+        f"UID={USERNAME};PWD={PASSWORD}"
+    )
+    return conn
+
+def  fetch_data(connection):
+    df = pd.read_sql(''' SELECT FirstName, LastName , StudentCode, Email, SN.Notes, SN.CreateDate as N'Ngày tạo ghi chú' From Students S
+LEFT JOIN StudentNotes SN ON  S.Id = SN.StudentId
+order by SN.CreateDate desc ''', connection)
+    return df
+
+if __name__ == "__main__":
+    # Test the database connection
+    try:
+        connection = get_connection()
+        data = fetch_data(connection)
+        print(data.head()) 
+        print("Connection successful!")
+        connection.close()
+    except Exception as e:
+        print(f"Connection failed: {e}")
